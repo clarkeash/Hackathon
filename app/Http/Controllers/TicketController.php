c77bc5b5381d292ffe88faf485ec826dc7c3dd11
Ashley Clarke <me@ashleyclarke.me>
2015-11-27 13:31:21 +0000

    start tickets

---
+++ app/Http/Controllers/TicketController.php
@@ -0,0 +1,105 @@
+<?php
+
+namespace OVH\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use OVH\Http\Requests;
+use OVH\Http\Controllers\Controller;
+use OVH\Ticket;
+use OVH\User;
+
+class TicketController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'name' => 'required',
+            'email' => 'required|email',
+            'subject' => 'required',
+        ]);
+
+        $name = $request->get('name');
+        $email = $request->get('email');
+
+        if(!$user = User::findByEmail($email))
+        {
+            $user = User::create(compact('name', 'email'));
+        }
+
+        Ticket::create([
+            'subject' => $request->get('subject'),
+        ]);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
