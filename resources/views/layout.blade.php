307e645710c93ad65c1f2c7152b74c476a15dcae
Ashley Clarke <me@ashleyclarke.me>
2015-11-27 16:41:07 +0000

    add js

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -68,7 +68,7 @@
 </footer>
 
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
-<script src="js/bootstrap.js" type="text/javascript"></script>
+<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.js" type="text/javascript"></script>
 </body>
 </html>
 
