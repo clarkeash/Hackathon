9e87a77c02e1cc306e03ea1a3a3ebe723d92e8cd
Steve <steve.axtmann@gmail.com>
2015-11-27 15:07:22 +0000

    added actual ticket show page

--- resources/views/tickets/show.blade.php
+++ resources/views/tickets/show.blade.php
@@ -1,27 +1,45 @@
 @extends('layout')
 
 @section('content')
-    Subject:
-    {{$ticket->subject}}
-    <br>
-    Status:
-    {{$ticket->status}}
-    <br>
-    Created:
-    {{$ticket->created_at}}
-    <br>
-    User:
-    {{\OVH\User::find($ticket->user_id)->name}}
-    <br><br>
-    Comments:
-    <br>
-    @foreach($comments as $comment)
-        {{$comment->person_type}}:
-        {{$comment->person->name}}
-        ({{$comment->created_at}})
-        <br>
-        {{$comment->content}}
-        <br>
-        <br>
-    @endforeach
+    <div class="col-xs-12 col-sm-12 col-lg-12">
+
+        <section class="latest-tickets">
+            <div class="ticket-subject">
+                {{$ticket->subject}}
+            </div>
+
+            <div class="ticket-status {{$ticket->status}}">
+                {{strtoupper($ticket->status)}}
+            </div>
+
+            <div class="ticket-body">
+                <p>{{$comments->first()->content}}</p>
+                <p>Created on {{$ticket->created_at}} by {{\OVH\User::find($ticket->user_id)->name}}</p>
+            </div>
+
+            <div class="ticket-replies">
+                @for($i = 1; $i < count($comments); $i++)
+                    <div class="reply-{{$comments[$i]->person_type == \OVH\Staff::class ? 'admin' : 'customer'}}">
+                        <div class="reply-header">
+                            <strong>{{$comments[$i]->person->name}} </strong> <small><em>replied on {{$comments[$i]->created_at}}</em></small>
+                        </div>
+                        <div class="reply-message">
+                            {{$comments[$i]->content}}
+                        </div>
+                    </div>
+                @endfor
+
+                <a href="close.html"><button class="btn btn-primary yellow" id="close" type="submit">Close the ticket</button></a>
+
+                <div class="repply">
+                    <h2>Reply</h2>
+                    <textarea id="new-ticket-body"></textarea>
+
+                    <button class="btn btn-primary" type="submit">Reply</button>
+                </div>
+            </div>
+
+        </section>
+
+    </div>
 @endsection
